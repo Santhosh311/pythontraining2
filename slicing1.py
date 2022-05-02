98f4f75a774a3421d7da9d1e956bc17a4c8a288b
Santhosh311 <dlssanthu88@gmail.com>
2022-05-02 17:04:54 +0530

    Committed messeage

---
+++ slicing1.py
@@ -0,0 +1,58 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+s="hello world"
+
+s[:8]
+'hello wo'
+s[2:-3]
+'llo wo'
+s[:5]
+'hello'
+s[6:]
+'world'
+s[:]
+'hello world'
+s[1:-1]
+'ello worl'
+s[8:1]
+''
+s[-2:-7]
+''
+s[-10:8]
+'ello wo'
+s[1:9:2]
+'el o'
+s[::2]
+'hlowrd'
+s[8:-10:-1]
+'row oll'
+s[::-1]
+'dlrow olleh'
+s[::-2]
+'drwolh'
+s[2:20]
+'llo world'
+s[7:-4]
+''
+s[9:-3]
+''
+s[-10:-3]
+'ello wo'
+s[-7:0]
+''
+s[:0:-1]
+'dlrow olle'
+s[20:0:-1]
+'dlrow olle'
+s[20::]
+''
+s[20:2:-1]
+'dlrow ol'
+s[7:-11:-1]
+'ow olle'
+s[200:2:-1]
+'dlrow ol'
+s[-10:-2:-1]
+''
+s[-10:2:1]
+'e'
